87fa24130706aa68a3e0a1f62b836fbc2bff55cb
OlgaMoisevich <ol.ivochka@yandex.ru>
2021-03-11 12:36:34 +0300

    feat: add the solution task of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let arr = Object.values(str.split(''));
+    if (arr.length % 2 !== 0) {
+        return false;
+    }
+
+    let open_sign = [];
+    let close_sign = [];
+    let stack = [];
+    let openIndex;
+    let closeIndex;
+
+    bracketsConfig.forEach(([key, value]) => {
+        open_sign.push(key);
+        close_sign.push(value)
+    });
+
+    for (let sign = 0; sign < arr.length; sign++) {
+
+        closeIndex = close_sign.indexOf(arr[sign]);
+        if (closeIndex !== -1 && stack.length) {
+            let last_el = stack[stack.length - 1];
+            if (last_el === open_sign[closeIndex]) {
+                stack.pop();
+                continue
+            }
+        }
+
+        openIndex = open_sign.indexOf(arr[sign]);
+        if (openIndex !== -1) {
+            stack.push(arr[sign]);
+        } else {
+            return false
+        }
+    };
+    return true
+};
+
+
